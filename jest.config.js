86f6e02a103c947b7bf2b851255c16831bf7d7d2
Paul <smartwp@126.com>
2019-03-25 16:34:18 +0800

    rename

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  testRegex: '(/test/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
