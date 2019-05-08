adc0bdae14b65023223316c89c8715eecaa62d7c
Paul <smartwp@126.com>
2019-05-08 14:02:40 +0800

    add bundle tool

--- src/example/main.ts
+++ src/example/main.ts
@@ -1,6 +1,5 @@
-import { Client } from "../index";
 
-console.log("Start")
+import { Client } from "../index";
 const client = new Client("ws://127.0.0.1:8081", null)
 
-console.log("client")
+console.log("test", "a")
